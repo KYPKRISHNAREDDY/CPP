a04c634348bad0d71c867e858e6c86099778166e
KYPKRISHNAREDDY <kypkrishna777@gmail.com>
2023-05-03 16:51:21 +0530

    CPP BASICS

---
+++ 1.Basics/1.1Things to know in CPP/basics.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+    cout<<"Hello, world!"<<endl;
+   return 0;
+}
+
+
+
+/*
+<iostream> bits::cout<<;,using namespace std; for input and output
+<bits/stdc++.h> to include all libraries ex:string etc...
+int(4)=  -10^9 to 10^9,long (4) = -+10^12 ,longlong(8)=-+10^18
+float(4),double(8)
+char c='g';//Single Quotes
+
+cin>>str; //differentiates using space
+getline(cin,str);//differentiates using newLine
+
+if(){}
+else if(){}
+else{}
+
+switch(){}
+do{}while();
+
+pass by reference: int maxi(int &i){}
+
+Time complexity: !=Time Taken
+it is rate at which the time taken increases with respect to input size
+BigOh-WorstCase Scenario,Avoid Constants
+Omega-BestCase
+Theta-Avg case scenario
+
+In competitive Programmig servers, the time taken for 10^8 operations is 1sec
+2sec-> 2*10^8 Operations
+*/
